9c5854ad56d174aa500cf318b130f6fcc86eca1d
LEE <zhangloo333@gmail.com>
2020-03-22 15:36:29 -0700

    webpack babel, dev-server setup

---
+++ designPattern/webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugIn = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path:__dirname,
+        filename: './release/bundle.js'
+    },
+    //translate es6 to es5
+    module:{
+        rules:[{
+            test: /\.js?$/,
+            exclude:/(node_modules)/,
+            loader:'babel-loader'
+        }]
+    },
+
+    //建立一个插件裂变，然后把 调用 package json的东西，新建一个列表
+    plugins: [
+        new HtmlWebpackPlugIn({
+            template:'./index.html'
+        })
+    ],
+    devServer : {
+        contentBase: path.join(__dirname, './release'), //根目录
+        open:true, //自动打开预览器
+        port: 9000
+    }
+}
