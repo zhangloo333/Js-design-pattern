8e1eed8bc55e507546a997085ab6887aeb2503a5
LEE <zhangloo333@gmail.com>
2020-03-22 21:34:14 -0700

    singleton pattern

--- designPattern/src/index.js
+++ designPattern/src/index.js
@@ -1,12 +1,26 @@
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    getName(){
-        return this.name;
+class SingleObject {
+    login() {
+        console.log('login.....');
     }
 }
 
-let person = new Person("lee");
-alert(person.getName());
+SingleObject.getInstance = (function() {
+    let instance
+    return function() {
+        if(!instance) {
+            instance = new SingleObject();
+        }
+        return instance
+    }
+})()
+
+let obj1= SingleObject.getInstance()
+obj1.login()
+let obj2 = SingleObject.getInstance()
+obj2.login()
+
+console.log('object1 === object2', obj1 === obj2);
+
+//如果别人不知道我们的使用方法，他就是去new 了一个。
+let obj3 = new SingleObject();
+console.log('object1 === object2', obj2 === obj3);
