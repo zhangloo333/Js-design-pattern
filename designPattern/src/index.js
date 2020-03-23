0b2e59970f126994b151e805fa547a4daee860be
LEE <zhangloo333@gmail.com>
2020-03-22 17:53:57 -0700

    object oriented introduction

--- designPattern/src/index.js
+++ designPattern/src/index.js
@@ -1,6 +1,15 @@
-class Person {
-    constructor(name) {
+class People {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        alert(`${this.name} eat something`);
+    }
+
+    speak() {
+        alert(`My name is ${this.name}, age ${this.age}`);
     }
 
     getName(){
@@ -8,5 +17,22 @@ class Person {
     }
 }
 
-let person = new Person("lee");
-alert(person.getName());
+//子类 继承父类
+class Student extends People {
+    constructor(name, age, number) {
+        super(name, age); //super 调用父类里面的name， and age， 因为number没有，所以不传入
+        this.number = number;
+    }
+
+    study() {
+        alert(`${this.name} study`)
+    }
+}
+
+let xiaoming = new Student('xiaoming', 10, 'A1');
+xiaoming.study()
+alert(xiaoming.number);
+xiaoming.eat();
+let xiaohong = new Student('xiaohong', 11, 'A2');
+xiaohong.study()
+xiaohong.speak();
