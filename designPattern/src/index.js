d813d4defa7da02d427e3ceb184eee0aa56af538
LEE <zhangloo333@gmail.com>
2020-03-28 11:02:07 -0700

    design patternB_1_code

--- designPattern/src/index.js
+++ designPattern/src/index.js
@@ -1,12 +1,27 @@
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    getName(){
-        return this.name;
+class Circle {
+    draw() {
+        console.log('draw a cicle');
     }
 }
 
-let person = new Person("lee");
-alert(person.getName());
+class Decorator {
+    constructor(circle) {
+        this.circle = circle
+    }
+    draw() {
+        this.circle.draw
+        this.setRedBorder(this.circle)
+    }
+    setRedBorder(circle) {
+      console.log('set red boarder')
+    }
+}
+
+//test
+
+let circle = new Circle()
+circle.draw();
+
+console.log('-----分割线-----')
+let dec = new Decorator(circle)
+dec.draw();
