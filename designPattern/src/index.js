5c4f262101be1bb4e38dff26019ded1a16da0bfc
LEE <zhangloo333@gmail.com>
2020-03-28 10:42:20 -0700

    siglton pattern

--- designPattern/src/index.js
+++ designPattern/src/index.js
@@ -1,12 +1,20 @@
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    getName(){
-        return this.name;
+class Adaptee {
+    specificRequest() {
+        return '德国标准插头'
     }
 }
 
-let person = new Person("lee");
-alert(person.getName());
+class Target {
+    constructor() {
+        this.adaptee = new Adaptee()
+    }
+    request() {
+        let info = this.adaptee.specificRequest()
+        return `${info} - 转换器 -> 中国标准插头`
+    }
+}
+
+//test
+let target = new Target();
+let res = target.request();
+console.log(res)
